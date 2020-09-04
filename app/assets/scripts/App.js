7abd5322fe19c3326cce36ceae5ad2c7e5f54be6
sridhar-reddy <sridharreddy.93953@gmail.com>
2020-09-04 21:54:30 +0530

    Added clents area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,7 @@ import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import ClientSecretArea from "./modules/ClientSecretArea";
 
 
 
@@ -10,6 +11,7 @@ new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new StickyHeader();
+new ClientSecretArea();
 let modal;
 
 document.querySelectorAll(".open-modal").forEach(el => el.addEventListener("click", e => {
