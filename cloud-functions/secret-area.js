6bd5e60e92606361c764bbd746e0f6d10d049bbf
sridhar-reddy <sridharreddy.93953@gmail.com>
2020-09-04 20:56:10 +0530

    Updated the cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(event, context, callback){
-    callback(null, {
-        statusCode : 200,
-        body : "Welcome to the super secret area"
-    });
+    let body;
+    let bodyContent = `
+        <h3>Welcome to the secret area</h3>
+        <p>Here We can tell you that the sky is <strong>blue</strong>, and two plus two equals to four.</p>
+    `;
+
+    if(event.body){
+        body = JSON.parse(event.body);
+    } else {
+        body = {}
+    }
+
+    if(body.password === "javascript"){
+        callback(null, {
+            statusCode : 200,
+            body : bodyContent
+        });
+    } else {
+        callback(null, {
+            statusCode : 401
+        });
+    }
+
+    
 }
